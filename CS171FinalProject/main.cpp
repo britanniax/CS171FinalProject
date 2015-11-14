13279c9cd5766d733f0859e82f1ef96470650cae
Michael Gamlem III <mgamlem3@gmail.com>
2015-11-13 16:16:39 -0800

    Initial Commit

---
+++ CS171FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS171FinalProject
+//
+//  Created by Michael Gamlem on 11/13/15.
+//  Copyright © 2015 Michael Gamlem. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
